1b539dceb82852a86733f35e7e177c7eeec28893
Devraj Rana <devrajnara@gmail.com>
2025-07-16 14:14:51 +0000

    use good fonts colours and use annimation g sap 3 js and other

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-code antialiased">
+      <body className="antialiased">
         {children}
         <Toaster />
       </body>
